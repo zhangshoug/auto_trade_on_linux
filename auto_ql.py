b3c9761a15174727e62f161cf31dc90796c78303
zhangshoug <39645806+zhangshoug@users.noreply.github.com>
2018-07-28 21:06:59 +0800

    Update auto_ql.py

--- auto_ql.py
+++ auto_ql.py
@@ -45,7 +45,7 @@ while freq <3 :
     freq += 1
 #  ENTER键
 pyautogui.press('enter')
-time.sleep(19)
+time.sleep(55)
 ###登录 已设为自动登录
 ##按纽在屏幕上的坐标region需要修改成适合自己机器的
 #region=(708, 413, 185, 42)
@@ -56,58 +56,63 @@ time.sleep(19)
 ##按纽在屏幕上的坐标region需要修改成适合自己机器的
 region=(355, 70, 80, 25)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
-time.sleep(2)
+pyautogui.click(x,y)
+time.sleep(1)
 ###权息资料 前打勾
 ##按纽在屏幕上的坐标region需要修改成适合自己机器的
 region=(285, 385, 65, 15)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
-time.sleep(2)
+pyautogui.click(x,y)
+time.sleep(1)
 ### 添加
 ##按纽在屏幕上的坐标region需要修改成适合自己机器的
 region=(682, 195, 75, 30)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
+pyautogui.click(x,y)
 time.sleep(2)
 ###沪深市场
 ##按纽在屏幕上的坐标region需要修改成适合自己机器的
 region=(305, 190, 70, 15)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
+pyautogui.click(x,y)
 time.sleep(2)
 ### 全选 打勾
 region=(670, 170, 50, 20)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
+pyautogui.click(x,y)
 time.sleep(2)
 ### 确定
 region=(550, 420, 85, 30)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
-time.sleep(2)
+pyautogui.click(x,y)
+time.sleep(9)
 ###下载
 region=(665, 350, 100, 28)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
-time.sleep(2)
+pyautogui.click(x,y)
+time.sleep(29)
+test = 'None'
+###下载结束的图片需要事先生成
+while test == 'None' :
+    test = pyautogui.locateCenterOnScreen('image/ql_down_end.png')
+    time.sleep(5)
 ### 关闭 数据维护
 region=(750, 75, 12, 12)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
+pyautogui.click(x,y)
 time.sleep(2)
 ### 关闭 钱龙旗舰
 region=(1001, 28, 15, 15)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
+pyautogui.click(x,y)
 time.sleep(2)
 ### 直接退出
 region=(655, 325, 90, 25)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
+pyautogui.click(x,y)
 time.sleep(2)
 ### 关闭广告
 region=(798, 78, 14, 14)
 x,y = pyautogui.center(region)
-pyautogui.doubleClick(x,y)
+pyautogui.click(x,y)
 time.sleep(2)
