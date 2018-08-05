b623dd51cc35a7d361ba7ddda18dd3553962af35
zhangshoug <39645806+zhangshoug@users.noreply.github.com>
2018-08-05 16:20:15 +0800

    Update auto_down_data.py
    增加导出自选股，供将后卖出时hikyuu计算

--- auto_down_data.py
+++ auto_down_data.py
@@ -62,3 +62,34 @@ x,y = pyautogui.center(region)
 pyautogui.click(x,y)
 ###等待足够长时间以便完成数据下载，我这里不超过40分钟
 time.sleep(2400)
+##导出自选股
+##按纽在屏幕上的坐标region需要修改成适合自己机器的
+region=(120, 24, 35 ,25)
+x,y = pyautogui.center(region)
+pyautogui.click(x,y)
+time.sleep(5)
+#十二次 向下键
+freq=0
+while freq <3 :
+    pyautogui.press('down')
+    time.sleep(0.5)
+    freq +=1
+#  ENTER键
+pyautogui.press('enter')
+time.sleep(3)
+#报表中所有数据
+region=(615, 380, 100,20)
+x,y = pyautogui.center(region)
+pyautogui.click(x,y)
+time.sleep(0.5)
+#导出
+region=(745, 475, 60,25)
+x,y = pyautogui.center(region)
+pyautogui.click(x,y)
+time.sleep(5)
+#导出成功
+region=(765, 320, 15,15)
+x,y = pyautogui.center(region)
+pyautogui.click(x,y)
+time.sleep(5)
+
